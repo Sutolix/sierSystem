49d3265c0f56dcbf5c8b346c26a095b045a1769f
Pocchi <56132780+Sutolix@users.noreply.github.com>
2019-12-18 12:56:26 -0300

    atualização de formato

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="lib/jquery/jquery.min.js"></script>
-    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
-    <script src="js/scripts.js"></script>
-    <script type="text/javascript" src="js/calculo.js"></script>
-  </body>
-</html>
--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-var input = document.getElementById('dataEntrega');
-input.addEventListener('change', function() {
-  var agora = new Date();
-  var escolhida = new Date(this.value);
-  if (escolhida < agora) this.value = [agora.getFullYear(), agora.getMonth() + 1, agora.getDate()].map(v => v < 10 ? '0' + v : v).join('-');
-});
